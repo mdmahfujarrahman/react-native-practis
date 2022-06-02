79f519969d3603058f907abee0bc2b8eea3bf36e
mdmahfujarrahmane <ahmedmahfuj28@gmail.comm>
2022-06-02 18:59:11 +0600

    input field added

--- components/Countrys.js
+++ components/Countrys.js
@@ -1,21 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 import Country from './Country';
 
 export default function Countrys() {
-  const [countrys, setCountry] = useState([]);
+    const [countrys, setCountry] = useState([]);
+    const [searched, setSearched] = useState([]);
 
   useEffect(() => {
       fetch("https://restcountries.com/v3.1/all")
           .then((res) => res.json())
-          .then((data) => setCountry(data));
+          .then((data) => {
+              setCountry(data)
+              setSearched(data);
+            });
   });
+
+  const handleText = text => {
+      const filter = countrys.filter(country => country.name.common.includes(text));
+      setSearched(filter);
+  }
 
   return (
       <View>
           <Text style={styles.header}>Total Country: {countrys?.length}</Text>
+          <TextInput onChangeText={handleText} style={styles.input}></TextInput>
           <ScrollView>
-              {countrys.map((country) => (
+              {searched.map((country) => (
                   <Country country={country} />
               ))}
           </ScrollView>
@@ -26,6 +36,12 @@ export default function Countrys() {
         marginTop: 40,
         fontSize: 40,
         color: "red",
-        textAlign: "center"
+        textAlign: "center",
     },
-})
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+});
